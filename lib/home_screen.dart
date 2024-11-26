bfd683966f380a64b2103c9b528e9f4617e33306
C0de871 <muhammadaydi3@gmail.com>
2024-11-26 23:36:58 +0300

    log out using blocconsumer

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login/bloc/auth_bloc.dart';
+import 'package:login/login_screen.dart';
+import 'package:login/widgets/gradient_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +10,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-    );
+        appBar: AppBar(),
+        body: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthInitial) {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+                (route) => false,
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is AuthSuccess) {
+              return Column(
+                children: [
+                  Center(
+                    child: Text(state.uid),
+                  ),
+                  GradientButton(onPressed: () {
+                    context.read<AuthBloc>().add(AuthLogoutRequested());
+                  }),
+                ],
+              );
+            } else {
+              return const Text("Unknown error");
+            }
+          },
+        ));
   }
 }
